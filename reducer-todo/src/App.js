598d496f877d590e5bd4fe568c518faa3a59e85b
Francis Bulus <notchera@gmail.com>
2019-10-08 11:42:35 +0100

    built out app component with imports

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
 
+import { initialState, todoReducer } from './reducers/todoReducer';
+
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import TodoClear from './components/TodoClear';
+
 function App() {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+
   return (
-    <div className="App">
-    
+    <div className='App'>
+      <h1>Reducer Todo App</h1>
+      <TodoForm dispatch={dispatch} />
+      <TodoList state={state} dispatch={dispatch} />
+      <TodoClear dispatch={dispatch} />
     </div>
   );
 }
